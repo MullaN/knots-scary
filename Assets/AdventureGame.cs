78dee7ef48e2ce6865ae6fb3ede821a8703940fe
Andrew Mullan <admullan@gmail.com>
2021-01-27 17:34:15 -0800

    added state

---
+++ Assets/AdventureGame.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AdventureGame : MonoBehaviour
+{
+    [SerializeField] Text textComponent;
+
+    // Start is called before the first frame update
+    void Start() {
+        textComponent.text = "This text was added programatically.";
+    }
+
+    // Update is called once per frame
+    void Update() {
+        
+    }
+}
